53f73d9b8581fa5eb826427e294091794cd8445b
Jimin Lee <jimin3422@gmail.com>
2015-09-14 21:19:50 -0400

    ...:(

--- caughtSpeeding/src/caughtSpeeding/Speeding.java
+++ caughtSpeeding/src/caughtSpeeding/Speeding.java
@@ -2,15 +2,39 @@ package caughtSpeeding;
 
 public class Speeding {
 	
-	public static void main(String[] args)
-	{
+	public static void main(String[] args){
+	 
+	
+	
+		 
 		System.out.print(caughtSpeeding(60, false)); // should return 0
 		System.out.print(caughtSpeeding(65, false)); // should return 1
 		System.out.print(caughtSpeeding(65, true)); // should return 0
-	}
+		System.out.print(caughtSpeeding(81, true)); // should return 2
+	} 
 	
-	public static int caughtSpeeding(int speed, boolean isBirthday)
-	{
-		return 0;
-	}
+	public static int caughtSpeeding(int speed, boolean isBirthday) {
+		if ( isBirthday == false) {
+			if (speed <= 60) {
+				return 0;
+			}
+			if (speed >= 61 && speed <= 80) {
+				return 1;}
+			if (speed >= 81) {
+				return 2;
+				
+				
+			}
+		if ( isBirthday == true) {
+			if (speed <= 65) {
+				return 0;}		
+				if (speed >= 66 && speed <= 85) {
+					return 1;}
+				if (speed >= 86) {
+					return 2;
+				}
+		}
+		} return 0;
+	} 
 }
+
